9e98f6ca5ad33a2061205b66e5a0344b93172fd0
arthvelc <asolares.devml@gmail.com>
2024-05-30 17:32:06 -0600

    practica 10

---
+++ practica 10/Main (1).java
@@ -0,0 +1,126 @@
+interface Figure {
+    String getColor();
+    double perimetro();
+    double area();
+}
+
+class Triangle implements Figure {
+    private String color;
+    private double a, b, c;
+
+    public Triangle(String color, double a, double b, double c) {
+        this.color = color;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public double perimetro() {
+        return a + b + c;
+    }
+
+    @Override
+    public double area() {
+        double s = perimetro() / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
+
+class Circle implements Figure {
+    private String color;
+    private double radius;
+
+    public Circle(String color, double radius) {
+        this.color = color;
+        this.radius = radius;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public double perimetro() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle implements Figure {
+    private String color;
+    private double width, height;
+
+    public Rectangle(String color, double width, double height) {
+        this.color = color;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public double perimetro() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
+
+class Hexagon implements Figure {
+    private String color;
+    private double side;
+
+    public Hexagon(String color, double side) {
+        this.color = color;
+        this.side = side;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public double perimetro() {
+        return 6 * side;
+    }
+
+    @Override
+    public double area() {
+        return (3 * Math.sqrt(3) * side * side) / 2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Figure[] figures = {
+            new Triangle("Red", 3, 4, 5),
+            new Circle("Blue", 10),
+            new Rectangle("Green", 4, 7),
+            new Hexagon("Yellow", 6)
+        };
+
+        for (Figure figure : figures) {
+            System.out.println("Color: " + figure.getColor() +
+                ", Perimeter: " + figure.perimetro() +
+                ", Area: " + figure.area());
+        }
+    }
+}
---
+++ practica 10/main (1).py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+import math
+
+class Figure(ABC):
+    @abstractmethod
+    def getColor(self):
+        pass
+
+    @abstractmethod
+    def perimetro(self):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Triangle(Figure):
+    def __init__(self, color, a, b, c):
+        self.color = color
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def getColor(self):
+        return self.color
+
+    def perimetro(self):
+        return self.a + self.b + self.c
+
+    def area(self):
+        s = self.perimetro() / 2
+        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+
+class Circle(Figure):
+    def __init__(self, color, radius):
+        self.color = color
+        self.radius = radius
+
+    def getColor(self):
+        return self.color
+
+    def perimetro(self):
+        return 2 * math.pi * self.radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+class Rectangle(Figure):
+    def __init__(self, color, width, height):
+        self.color = color
+        self.width = width
+        self.height = height
+
+    def getColor(self):
+        return self.color
+
+    def perimetro(self):
+        return 2 * (self.width + self.height)
+
+    def area(self):
+        return self.width * self.height
+
+class Hexagon(Figure):
+    def __init__(self, color, side):
+        self.color = color
+        self.side = side
+
+    def getColor(self):
+        return self.color
+
+    def perimetro(self):
+        return 6 * self.side
+
+    def area(self):
+        return (3 * math.sqrt(3) * self.side**2) / 2
+
+# Demonstración de polimorfismo
+figures = [
+    Triangle("Red", 3, 4, 5),
+    Circle("Blue", 10),
+    Rectangle("Green", 4, 7),
+    Hexagon("Yellow", 6)
+]
+
+for figure in figures:
+    print(f"Color: {figure.getColor()}, Perimeter: {figure.perimetro()}, Area: {figure.area()}")
